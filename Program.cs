a8305f6550f2bca62490f888446b4463e0b9250a
AdrianMartinezGit <adrionmtz02@gmail.com>
2024-04-30 09:38:02 -0700

    Add/Remove Files

--- Program.cs
+++ Program.cs
@@ -2,6 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// This is how we’re connecting our database to API
+var connectionString = builder.Configuration.GetConnectionString("MyTaskTrackerString");
+
+//configures entity framework core to use SQL server as the database provider for  a datacontext DbContext in our project
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
