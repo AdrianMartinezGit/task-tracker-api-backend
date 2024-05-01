49dabfabe90f5cc8ae33b0a21cad9c2f55117075
AdrianMartinezGit <adrionmtz02@gmail.com>
2024-05-01 11:12:18 -0700

    Add Azure Resource

--- Program.cs
+++ Program.cs
@@ -11,12 +11,12 @@ builder.Services.AddScoped<UserService>();
 // This is how we’re connecting our database to API
 var connectionString = builder.Configuration.GetConnectionString("MyTaskTrackerString");
 
-//configures entity framework core to use SQL server as the database provider for  a datacontext DbContext in our project
+//configures entity framework core to use SQL server as the database provider for a datacontext DbContext in our project
 builder.Services.AddDbContext<DataContext>(Options => Options.UseSqlServer(connectionString));
 
 builder.Services.AddCors(options => options.AddPolicy("TaskTrackerPolicy", 
 builder => {
-    builder.WithOrigins("http://localhost:5244")
+    builder.WithOrigins("http://localhost:5074")
     .AllowAnyHeader()
     .AllowAnyMethod();
 }
