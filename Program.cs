5c0fccf9f613b4de6ecc1d09b7827786735c4b16
AdrianMartinezGit <adrionmtz02@gmail.com>
2024-05-03 09:36:34 -0700

    Add Vercel URL

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddDbContext<DataContext>(Options => Options.UseSqlServer(conne
 
 builder.Services.AddCors(options => options.AddPolicy("TaskTrackerPolicy", 
 builder => {
-    builder.WithOrigins("http://localhost:5074", "http://localhost:3000")
+    builder.WithOrigins("http://localhost:5074", "http://localhost:3000", "https://tasktrackerfullstack.vercel.app")
     .AllowAnyHeader()
     .AllowAnyMethod();
 }
